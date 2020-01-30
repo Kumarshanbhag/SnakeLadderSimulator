ca25d5bb72171efe642a5b5114b754733bded599
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-01-30 14:53:38 +0530

    ADDED : Random Function Used To check Options(NO_Play,Snake,Ladder)

--- snakeLadderSimulation.sh
+++ snakeLadderSimulation.sh
@@ -1,4 +1,26 @@
 #!/bin/bash -x
 echo "Welcome To Snake And Ladder Simulator"
+NO_PLAY=1
+LADDER=2
+SNAKE=3
+
+function CheckOption() {
+case $1 in
+	$NO_PLAY)
+		position=$position
+	;;
+	$LADDER)
+		position=$(($position+$2))
+	;;
+	$SNAKE)
+		position=$(($position-$2))
+	;;
+esac
+}
+
+function RandomCalculate() {
+CheckOption $((RANDOM%$1+1)) $((RANDOM%$2+1))
+}
+
 position=0
-echo "Die Rolled $((RANDOM%6+1))"
+RandomCalculate 3 6
