f1d4d86913f24f3749a7c475a99f06f1a07e816b
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-01 11:57:08 +0530

    ADDED : Dictionary Used For Diplaying Die Count And Position

--- snakeLadderSimulation.sh
+++ snakeLadderSimulation.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
+#!/bin/bash -x
+declare -A player
 echo "Welcome To Snake And Ladder Simulator"
 NO_PLAY=1
 LADDER=2
@@ -7,8 +8,9 @@ WIN_POSITION=100
 START_POSITION=0
 
 function CheckOption() {
-while (( $position < $WIN_POSITION ))
+while [[ $position -ne $WIN_POSITION ]]
 do
+((count++))
 	case $1 in
 		$NO_PLAY)
 			position=$position
@@ -28,6 +30,7 @@ do
 			fi
 		;;
 	esac
+player[$count]=$position
 RandomCalculate 3 6
 done
 }
@@ -36,5 +39,15 @@ function RandomCalculate() {
 CheckOption $((RANDOM%$1+1)) $((RANDOM%$2+1))
 }
 
+function DisplayDieAndPosition() {
+for die in ${!player[@]}
+do
+	echo "Die $die : ${player[$die]}"
+done | sort -k2 -n 
+}
+
 position=0
+count=0
+player[$count]=$position
 RandomCalculate 3 6
+DisplayDieAndPosition
